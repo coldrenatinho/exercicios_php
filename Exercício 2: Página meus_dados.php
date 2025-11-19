8d3c993fa14c364364bdf9ac54d49c2bdfd2091d
Renato A. Silva <araujorenato045gmail.com>
2025-11-18 20:14:36 -0400

    Ex 2

---
+++ Exercício 2: Página meus_dados.php
@@ -0,0 +1,14 @@
+<?php
+// Exercício 2: Informações pessoais
+$nome = "Seu Nome";
+$idade = 25;
+$cidade = "Sua Cidade";
+$hobby = "Seu Hobby";
+$curso = "Seu Curso";
+
+echo "Nome: $nome<br>";
+echo "Idade: $idade anos<br>";
+echo "Cidade: $cidade<br>";
+echo "Hobby: $hobby<br>";
+echo "Curso: $curso<br>";
+?>
